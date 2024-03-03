d3bdc6e526316812d7da78b8c25d813b17415123
Alina-Pishak <alinapishak@icloud.com>
2024-03-03 21:34:45 +0200

    clear extra code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,24 +28,6 @@ const Header = () => {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Medicine
-          </Typography> */}
-
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -91,24 +73,6 @@ const Header = () => {
               ))}
             </Menu>
           </Box>
-          {/* <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Medicine
-          </Typography> */}
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
--- src/components/ShopList/ShopList.jsx
+++ src/components/ShopList/ShopList.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { Grid } from "@mui/material";
 import ShopListItem from "../ShopListItem/ShopListItem";
 
